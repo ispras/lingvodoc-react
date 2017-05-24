a1dfd01d74157faebfd3b435129bc231d623019b
alexlag <bo4onok@gmail.com>
2017-05-24 13:10:25 +0300

    determinated color for strings

---
+++ src/utils/string.js
@@ -0,0 +1,17 @@
+export function hashCode(str) {
+  // eslint-disable-next-line no-bitwise
+  return Array.from(str).reduce((hash, char) => char.charCodeAt(0) + ((hash << 5) - hash), 0);
+}
+
+export function intToRGB(i) {
+  // eslint-disable-next-line no-bitwise
+  const c = (i & 0x00FFFFFF).toString(16).toUpperCase();
+
+  return '00000'.substring(0, 6 - c.length) + c;
+}
+
+export function styleFor(str) {
+  return {
+    color: `#${intToRGB(hashCode(str))}`,
+  };
+}
