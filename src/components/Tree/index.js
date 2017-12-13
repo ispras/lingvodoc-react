60983ff40e3e646326e240cf35c763be55cf24f6
alexlag <bo4onok@gmail.com>
2017-12-13 20:25:38 +0300

    better dict tree view

--- src/components/Tree/index.js
+++ src/components/Tree/index.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import SortableTree, { map } from 'react-sortable-tree';
 
+function stateFromProps({ data, expanded }) {
+  return {
+    tree: map({
+      treeData: data.toJS(),
+      callback: ({ node }) => ({ ...node, expanded: !!expanded }),
+      getNodeKey: ({ treeIndex }) => treeIndex,
+      ignoreCollapsed: false,
+    }),
+  };
+}
+
 class LanguageTree extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      tree: map({
-        treeData: props.data.toJS(),
-        callback: ({ node }) => ({ ...node, expanded: !!props.expanded }),
-        getNodeKey: ({ treeIndex }) => treeIndex,
-        ignoreCollapsed: false,
-      }),
-    };
+    this.state = stateFromProps(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(stateFromProps(nextProps));
   }
 
   render() {
     return (
-      <div style={{ height: 600 }}>
-        <SortableTree
-          canDrag={false}
-          rowHeight={42}
-          scaffoldBlockPxWidth={32}
-          treeData={this.state.tree}
-          onChange={tree => this.setState({ tree })}
-          generateNodeProps={this.props.generateNodeProps}
-        />
-      </div>
+      <SortableTree
+        canDrag={false}
+        rowHeight={42}
+        scaffoldBlockPxWidth={32}
+        treeData={this.state.tree}
+        onChange={tree => this.setState({ tree })}
+        generateNodeProps={this.props.generateNodeProps}
+      />
     );
   }
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,47 +1,137 @@
 import React from 'react';
-import { fromJS, List, Map } from 'immutable';
-import { Container } from 'semantic-ui-react';
+import { withReducer } from 'recompose';
+import { fromJS, List, Map, Set } from 'immutable';
+import { Link } from 'react-router-dom';
+import { Container, Dropdown } from 'semantic-ui-react';
 
 import LanguageTree from 'components/Tree';
 
 import { languagesTree } from '../Search';
 import { assignDictsToTree } from '../Search/treeBuilder';
 
+import './published.scss';
+
 const DICTS = fromJS(require('./dicts.json')).reduce((acc, dict) => acc.set(dict.get('id'), dict), new Map());
 const GRANTS = fromJS(require('./grants.json'));
 
-function generateNodeProps({ node, path }) {
-  switch (node.type) {
-    case 'language':
-      return {
-        title: node.translation,
-      };
+function toId(arr, prefix = null) {
+  const joiner = prefix ? arr[prefix] : arr;
+  return joiner.join('/');
+}
+
+const Perspective =
+({
+  parent: pid,
+  perspective: p,
+}) =>
+  <Dropdown.Item as={Link} to={`dictionary/${toId(pid)}/perspective/${toId(p.id)}`}>
+    {p.translation}
+  </Dropdown.Item>;
+
+const Dictionary = ({
+  id,
+  selected,
+  translation,
+  additional_metadata: meta,
+  perspectives,
+  onSelect,
+}) =>
+  <div className={`dict ${selected ? 'selected' : ''}`}>
+    <span className="dict-name" onClick={() => onSelect(id)}>{translation}</span>
+    <span className="dict-authors">{(meta && meta.authors) || '.'}</span>
+    <Dropdown className="dict-perspectives" inline text={`View (${perspectives.length})`}>
+      <Dropdown.Menu>
+        {
+          perspectives.map(pers =>
+            <Perspective
+              key={toId(pers.id)}
+              parent={id}
+              perspective={pers}
+            />
+          )
+        }
+      </Dropdown.Menu>
+    </Dropdown>
+  </div>;
+
+function generateNodeProps(onSelect) {
+  return ({ node, path }) => {
+    switch (node.type) {
+      case 'language':
+        return {
+          title: node.translation,
+        };
+      default:
+        return {
+          className: node.selected ? 'selected' : '',
+          title: <Dictionary {...node} onSelect={onSelect} />,
+        };
+    }
+  };
+}
+
+
+function restDictionaries(dicts, grants) {
+  const grantedDicts = grants.flatMap(grant => grant.getIn(['additional_metadata', 'participant']) || new List()).toSet();
+  return dicts.reduce((acc, dict, id) => grantedDicts.has(id) ? acc : acc.push(dict), new List());
+}
+
+function GrantedTree({ tree, onSelect }) {
+  if (tree.size === 0) {
+    return <div>Empty</div>;
+  }
+  return <LanguageTree expanded data={tree} generateNodeProps={generateNodeProps(onSelect)} />;
+}
+
+function selectedReducer(state, { type, payload }) {
+  const id = fromJS(payload);
+  switch (type) {
+    case 'TOGGLE_DICT':
+      return state.has(id) ? state.delete(id) : state.add(id);
     default:
-      return {
-        title: 'TODO',
-      };
+      return state;
   }
 }
 
-export default function () {
+function GrantedDicts({ selected, dispatch }) {
+  console.log(selected.toJS());
+
+  const dicts = DICTS.map((d, id) => d.set('selected', selected.get(id)));
+
   const trees = GRANTS
     .map((grant) => {
       const dictIds = grant.getIn(['additional_metadata', 'participant']) || new List();
-      const dicts = dictIds.map(id => DICTS.get(id));
-      return assignDictsToTree(dicts, languagesTree);
+      const pickedDicts = dictIds.map(id => dicts.get(id));
+      return {
+        id: grant.get('id'),
+        text: grant.get('translation'),
+        tree: assignDictsToTree(pickedDicts, languagesTree),
+      };
     });
+
+  const restTree = assignDictsToTree(restDictionaries(dicts, GRANTS), languagesTree);
+
+  function onSelect(payload) {
+    dispatch({ type: 'TOGGLE_DICT', payload });
+  }
 
   return (
     <Container className="published">
       {
-        trees.map(tree =>
-          <LanguageTree
-            expanded
-            data={tree}
-            generateNodeProps={generateNodeProps}
-          />
+        trees.map(({ id, text, tree }) =>
+          <div key={id} className="grant">
+            <div className="grant-title">{text}</div>
+            <GrantedTree tree={tree} onSelect={onSelect} />
+          </div>
         )
       }
+      <hr />
+      <div className="grant">
+        <div className="grant-title">Индивидуальная работа</div>
+        <GrantedTree tree={restTree} onSelect={onSelect} />
+      </div>
     </Container>
   );
 }
+
+export default withReducer('selected', 'dispatch', selectedReducer, new Set())(GrantedDicts);
