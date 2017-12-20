3e98d5cc65cc19aa760de33353c6fb945aaa0f4c
alexlag <bo4onok@gmail.com>
2017-12-20 14:20:00 +0300

    rel #16. reset apollo on locale change

--- src/sagas/locale.js
+++ src/sagas/locale.js
@@ -1,13 +1,19 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, takeEvery, select } from 'redux-saga/effects';
 import { getLocales } from 'api/locale';
-import { setLocales, requestLocales } from 'ducks/locale';
+import { setLocales, requestLocales, SELECT } from 'ducks/locale';
 import { err } from 'ducks/snackbar';
+
+export function* resetApollo() {
+  const client = yield select(state => state.apolloClient);
+  yield call(client.resetStore);
+}
 
 export default function* languageInit() {
   yield put(requestLocales());
   const response = yield call(getLocales);
   if (response.data) {
     yield put(setLocales(response.data));
+    yield takeEvery(SELECT, resetApollo);
   } else {
     yield put(err('Could not get locales'));
   }
