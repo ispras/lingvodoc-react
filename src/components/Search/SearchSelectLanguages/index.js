7357217c86a261c28c34b561fec863fad5214367
rav <rav@em70.ru>
2018-10-24 10:44:05 +0700

    Добавил запрос перевода кнопки Select languages с АПИ

--- src/components/Search/SearchSelectLanguages/index.js
+++ src/components/Search/SearchSelectLanguages/index.js
@@ -8,27 +8,11 @@ import { fromJS } from 'immutable';
 import { buildLanguageTree } from 'pages/Search/treeBuilder';
 import LanguageTree from './LanguageTree';
 
-/* ----------- QUERIES ----------- */
-const LanguagesWithDictionariesQuery = gql`
-  query Languages {
-    language_tree {
-      id
-      parent_id
-      translation
-      dictionaries {
-        id
-        parent_id
-        translation
-        category
-      }
-    }
-  }
-`;
-
 /* ----------- COMPONENT ----------- */
 class SearchSelectLanguages extends PureComponent {
   static propTypes = {
     data: PropTypes.array.isRequired,
+    translations: PropTypes.array.isRequired,
   }
 
   constructor() {
@@ -76,12 +60,13 @@ class SearchSelectLanguages extends PureComponent {
     const { data } = this.props;
     const { selectedLangs } = this.state;
     const selectedLangsCount = selectedLangs.length;
+    const buttonTranslation = this.props.translations[0].translation;
 
     return (
       <Segment.Group>
         <Segment>
           <Button primary basic fluid onClick={this.onShowLangsButtonClick}>
-            Select languages
+            {buttonTranslation}
             <strong> {selectedLangsCount > 0 ? `(selected ${selectedLangsCount} languages)` : null}</strong>
           </Button>
         </Segment>
@@ -100,24 +85,72 @@ class SearchSelectLanguages extends PureComponent {
 }
 
 const SearchSelectLanguagesWrap = (props) => {
-  const { data } = props;
-  const { error, loading } = data;
+  const { languagesQuery, translationsQuery } = props;
+  const { error: languagesQueryError, loading: languagesQueryLoading } = languagesQuery;
+  const { error: translationsQueryError, loading: translationsQueryLoading } = translationsQuery;
+  let translations = null;
 
-  if (error || loading) {
+  if (languagesQueryError || languagesQueryLoading) {
     return null;
+  }
+
+  if (translationsQueryError || translationsQueryLoading) {
+    translations = [{
+      translation: 'Search languages',
+    }];
+  } else {
+    translations = translationsQuery.advanced_translation_search;
   }
 
   const newProps = {
     ...props,
-    data: buildLanguageTree(fromJS(props.data.language_tree)).toJS(),
+    data: buildLanguageTree(fromJS(props.languagesQuery.language_tree)).toJS(),
+    translations,
   };
 
   return <SearchSelectLanguages {...newProps} />;
 };
 
 SearchSelectLanguagesWrap.propTypes = {
-  data: PropTypes.object.isRequired,
+  languagesQuery: PropTypes.object.isRequired,
+  translationsQuery: PropTypes.object.isRequired,
   // onChange: PropTypes.func.isRequired,
 };
 
-export default compose(graphql(LanguagesWithDictionariesQuery))(SearchSelectLanguagesWrap);
+/* ----------- QUERIES ----------- */
+const LanguagesWithDictionariesQuery = gql`
+  query Languages {
+    language_tree {
+      id
+      parent_id
+      translation
+      dictionaries {
+        id
+        parent_id
+        translation
+        category
+      }
+    }
+  }
+`;
+
+const i18nQuery = gql`
+  query {
+    advanced_translation_search(
+      searchstrings: [
+        "Select languages"
+      ]
+    ) {
+      translation
+    }
+  }
+`;
+
+export default compose(
+  graphql(LanguagesWithDictionariesQuery, {
+    name: 'languagesQuery',
+  }),
+  graphql(i18nQuery, {
+    name: 'translationsQuery',
+  })
+)(SearchSelectLanguagesWrap);
