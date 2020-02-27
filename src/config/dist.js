14e7e04a614144b5844d069ace7e71498b1f61c6
Alexandra <naumova.alvl@gmail.com>
2020-02-27 11:19:36 +0700

    gix config graphql

--- src/config/dist.js
+++ src/config/dist.js
@@ -1,9 +1,9 @@
 import base from './base';
 
 const dist = {
-
-    apiUrl: 'http://localhost:9999',
-/*   apiUrl: 'http://10.10.11.137', */
+/* 
+    apiUrl: 'http://localhost:9999', */
+  apiUrl: 'http://10.10.11.137',
   env: 'dist',
   homePath: '/',
 };
--- src/graphql/index.js
+++ src/graphql/index.js
@@ -24,7 +24,7 @@ const errorLink = onError(({ networkError = {}, graphQLErrors }) => {
 
 const httpLink = createUploadLink({
 /*   uri: `${config.apiUrl}/graphql`, */
-uri: `http://localhost:9000/graphql`,
+uri: `http://10.10.11.137:9898/graphql`,
   credentials: 'same-origin',
 });
 
