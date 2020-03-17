13f4f838c78d16f7ce69702e16e7b505029bcf96
Andrey Zharov <jandrew@inbox.ru>
2020-03-17 18:17:50 +0300

    #181 - Lexical Entries roles and properties commands should be unavailable for unauthorized users.

--- src/pages/Perspective/PerspectivePath.js
+++ src/pages/Perspective/PerspectivePath.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import gql from 'graphql-tag';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
@@ -40,12 +41,8 @@ const queryAvailablePerspectives = gql`
  */
 class PerspectivePath extends React.Component {
 
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    const { id, dictionary_id, queryPerspectivePath, queryAvailablePerspectives, mode, className, actions } = this.props;
+    const { id, dictionary_id, queryPerspectivePath, queryAvailablePerspectives, mode, className, actions, user } = this.props;
 
     if (queryPerspectivePath.loading || queryPerspectivePath.error || queryAvailablePerspectives.loading || queryAvailablePerspectives.error) {
       return null;
@@ -61,15 +58,15 @@ class PerspectivePath extends React.Component {
           sections={tree.slice().reverse().map((e, index) => {
             return {
               key: e.id,
-              content: perspectives.length > 1 && index == tree.length - 1 ?
+              content: perspectives.length > 1 && index === tree.length - 1 ?
 
                 <Dropdown inline text={e.translation}>
                   <Dropdown.Menu>
-                    {perspectives.filter(pers => pers.id != tree[0].id).map(pers => (
+                    {perspectives.filter(pers => pers.id !== tree[0].id).map(pers => (
                       <Dropdown.Item
                         key={pers.id}
-                        as="a"
-                        href={window.location.protocol + '//' + window.location.host + `/dictionary/${pers.parent_id.join('/')}/perspective/${pers.id.join('/')}/${mode}`}
+                        as={Link}
+                        to={`/dictionary/${pers.parent_id.join('/')}/perspective/${pers.id.join('/')}/${mode}`}
                         icon="chevron right"
                         text={pers.translation}
                       />))
@@ -77,17 +74,23 @@ class PerspectivePath extends React.Component {
 
                     <Dropdown.Divider />
 
-                    <Dropdown.Item
-                      icon="users"
-                      text={getTranslation(`'${e.translation}' roles...`)}
-                      onClick={() => actions.openRoles(id, 'perspective')}
-                    />
-                    <Dropdown.Item
-                      icon="setting"
-                      text={getTranslation(`'${e.translation}' properties...`)}
-                      onClick={() =>
-                        actions.openPerspectivePropertiesModal(id, dictionary_id)}
-                    />
+                    { user.id !== undefined &&
+                      [
+                        <Dropdown.Item
+                          key="roles"
+                          icon="users"
+                          text={getTranslation(`'${e.translation}' roles...`)}
+                          onClick={() => actions.openRoles(id, 'perspective')}
+                        />,
+                        <Dropdown.Item
+                          key="properties"
+                          icon="setting"
+                          text={getTranslation(`'${e.translation}' properties...`)}
+                          onClick={() =>
+                            actions.openPerspectivePropertiesModal(id, dictionary_id)}
+                        />
+                      ]
+                    }
                     <Dropdown.Item
                       icon="percent"
                       text={getTranslation(`'${e.translation}' statistics...`)}
@@ -118,20 +121,17 @@ PerspectivePath.defaultProps = {
 
 export default compose(
   connect(
-    null,
+    state => state.user,
     dispatch => ({
       actions: bindActionCreators({ openRoles, openPerspectivePropertiesModal, openStatistics }, dispatch),
     })
   ),
   graphql(queryPerspectivePath, {
-      name: 'queryPerspectivePath',
-      options: (props) => ({ variables: { id: props.id } })
-    }
-  ),
+    name: 'queryPerspectivePath',
+    options: props => ({ variables: { id: props.id } })
+  }),
   graphql(queryAvailablePerspectives, {
-      name: 'queryAvailablePerspectives',
-      options: (props) => ({ variables: { dictionary_id: props.dictionary_id } })
-    }
-  )
-)
-(PerspectivePath);
+    name: 'queryAvailablePerspectives',
+    options: props => ({ variables: { dictionary_id: props.dictionary_id } })
+  })
+)(PerspectivePath);
