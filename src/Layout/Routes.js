cda64ce4bb1d5650de5ae465f317f492d857a38a
Alexuhugugul <thecrosssama@gmail.com>
2020-09-12 15:48:26 +0700

    delete news

--- src/Layout/Routes.js
+++ src/Layout/Routes.js
@@ -19,7 +19,7 @@ import Requests from 'pages/Requests';
 import EditTranslations from 'pages/EditTranslations';
 import Organizations from 'pages/Organizations';
 import TopSectionSelector from 'pages/TopSectionSelector';
-import NewsEditor from 'pages/NewsEditor';
+
 
 import DictionariesAll from 'pages/DictionaryAll';
 import TreeRoute from 'pages/TreeRoute';
@@ -61,7 +61,6 @@ const Routes = () => (
     <Route path="/supportRoute" component={SupportRoute} />
     <Route path="/no_grants" component={NoGrants} />
     <Route path="/corpora_all" component={CorporaAll} />
-    <Route path="/news_editor" component={NewsEditor}/>
     <Route component={NotFound} />
 
 
