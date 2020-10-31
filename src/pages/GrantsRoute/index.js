6e6a0df7191316e3deb72623bcd1e42a7cc99592
Alexuhugugul <thecrosssama@gmail.com>
2020-10-31 11:59:22 +0700

    delete sem-ui component

--- src/pages/GrantsRoute/index.js
+++ src/pages/GrantsRoute/index.js
@@ -6,7 +6,8 @@ import imageLegalDocument from '../../images/legal-document.png';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import { getTranslation } from 'api/i18n';
-import { Container, Segment, Header, Card } from 'semantic-ui-react';
+import { Segment } from 'semantic-ui-react';
+
 const grants = gql`
   query grants{
     grants {
@@ -20,11 +21,11 @@ const grants = gql`
   }
 `;
 function grantsRoute(props) {
-  const { data: { grants } } = props
+  const { data: { grants } } = props;
 
   function correctDate(date) {
-    const newDate = new Date(Number(date) * 1000)
-    return newDate.getFullYear()
+    const newDate = new Date(Number(date) * 1000);
+    return newDate.getFullYear();
   }
   return (
     <div>
@@ -32,15 +33,15 @@ function grantsRoute(props) {
         <div className="background-img" />
         <p className="grants">{getTranslation('Grants')}</p>
         <div className="img-block">
-          <Link to="/grants"  ><img className="img" src={imageScholarship} /></Link>
+          <Link to="/grants" ><img className="img" src={imageScholarship} /></Link>
           <p className="name">{getTranslation('Grants')}</p>
         </div >
         <div className="img-block" >
-          <Link to="/without_grants"  ><img className="img" src={imageLegalDocument} /></Link>
+          <Link to="/without_grants" ><img className="img" src={imageLegalDocument} /></Link>
           <p className="name">{getTranslation('Off-grant projects')}</p>
         </div>
         <Segment>
-          <h3 className="black">{getTranslation("Project funded by grants")}</h3>
+          <h3 className="black">{getTranslation('Project funded by grants')}</h3>
 
           <Segment>
             <p>
@@ -52,7 +53,7 @@ function grantsRoute(props) {
                 поиску с учетом геолокаций, загрузки словарей из настольной версии программы)
                 необходимо зарегистрироваться и войти в систему.
                 Кнопки регистрации/входа в систему находятся в правом верхнем углу страницы.
-       </b>
+              </b>
             </p>
             <p>
               <Link to="/desktop">Настольные приложения, связанные с системой, можно скачать в разделе Desktop software</Link>
@@ -60,14 +61,12 @@ function grantsRoute(props) {
           </Segment>
 
           <ul>
-            {(grants) && (grants.map((grant) => {
-              return (<li key={grant.id} style={{ margin: "0 0 5px 0" }} >
-                {"(" + grant.issuer + "  "}
-                {grant.grant_number + ")  "}
-                {grant.translation + "  "}
-                {correctDate(grant.begin) + "-" + correctDate(grant.end) + "  "}
-              </li>)
-            }))}
+            {(grants) && (grants.map(grant => (<li key={grant.id} style={{ margin: '0 0 5px 0' }} >
+              {`(${grant.issuer}  `}
+              {`${grant.grant_number})  `}
+              {`${grant.translation}  `}
+              {`${correctDate(grant.begin)}-${correctDate(grant.end)}  `}
+            </li>)))}
 
           </ul>
         </Segment>
