51292fa2b25d25cb83354e4fe75e5679da16d084
nataliauvarova <n.uvarova@ispras.ru>
2021-10-27 15:38:08 +0000

    fix tasks

--- src/components/TaskList/index.js
+++ src/components/TaskList/index.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { branch, renderComponent, lifecycle, compose } from 'recompose';
-import { List, Progress, Button, Segment, Header } from 'semantic-ui-react';
-
-import styled from 'styled-components';
+import { List, Progress, Button } from 'semantic-ui-react';
 
 import { removeTask } from 'ducks/task';
 import { run, stop } from 'ducks/saga';
@@ -14,16 +12,6 @@ import { getTranslation } from 'api/i18n';
 
 import imageEmpty from '../../images/no_data.svg';
 
-const OrWrapper = styled(Segment)`
-  .delete-task-button {
-    box-shadow: none !important;
-    position: absolute !important;
-    padding: 10px !important;
-    margin: 0;
-    right: 0;
-    top: 0;
-  }
-`;
 const Empty = () => (
   <div className="lingvo-sidebar__empty">
     <h3>{getTranslation('No background tasks')}</h3>
@@ -47,7 +35,7 @@ function Task(props) {
   } = props;
 
   const links = result_link_list.map(link => (
-    <div key={link} className="lingvo-tasks-link">
+    <div key={link} className="lingvo-task__link">
       <a href={link} key={link}>
         {link}
       </a>
@@ -56,14 +44,18 @@ function Task(props) {
 
   return (
     <List.Content>
-      <OrWrapper>
-        <Header size="small">{task_family}</Header>
-        <Button compact basic icon="delete" className="delete-task-button" onClick={() => remove(id)} />
-
-        {task_details}
-        <Progress label={`(${current_stage}/${total_stages}) ${status}`} percent={progress} progress="percent" />
-        {links}
-      </OrWrapper>
+      <div className="lingvo-task">
+        <div className="lingvo-task__title">{task_family}</div>
+        <Button className="lingvo-task__delete" onClick={() => remove(id)}>
+          <i className="lingvo-icon-close" />
+        </Button>
+        <div className="lingvo-task__content">
+          <div className="lingvo-task__details">{task_details}</div>
+          <Progress label={`(${current_stage}/${total_stages}) ${status}`} percent={progress} progress="percent" size="small" 
+            className={progress && (progress === 100) ? "lingvo-task__progress lingvo-task__progress_success" : "lingvo-task__progress" } />
+          {links}
+        </div>
+      </div>
 
       <List.Description />
     </List.Content>
@@ -73,7 +65,7 @@ function Task(props) {
 const Task1 = connect(null, dispatch => bindActionCreators({ removeTask }, dispatch))(Task);
 
 const TaskList = enhance(({ tasks }) => (
-  <List divided relaxed>
+  <List relaxed>
     {tasks.map(task => (
       <List.Item key={task.id}>
         <Task1 {...task} />
