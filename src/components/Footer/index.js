bd9442289e6ff400cad51aa3916e037aaa1dba0b
nataliauvarova <n.uvarova@ispras.ru>
2022-03-03 10:53:34 +0000

    part page organizations

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { getTranslation } from 'api/i18n';
 
 const Footer = () => (
     <div className="lingvodoc-page__footer lingvodoc-footer">
         <div className="lingvodoc-footer__inner">
-            Copyright © 2012-2021 &nbsp;<a href="https://iling-ran.ru" target="_blank">Institute of Linguistics Russian Academy of Sciences</a>, <a href="https://ispras.ru" target="_blank">Ivannikov Institute for System Programming of the Russian Academy of Sciences</a>
+            Copyright © 2012-2022 &nbsp;<a href="https://iling-ran.ru" target="_blank">{getTranslation('Institute of Linguistics Russian Academy of Sciences')}</a>, <a href="https://ispras.ru" target="_blank">{getTranslation('Ivannikov Institute for System Programming of the Russian Academy of Sciences')}</a>
         </div>
     </div>
 );
