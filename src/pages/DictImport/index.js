756e02af95f41782922990ecaf5c0925b5e90aff
nataliauvarova <n.uvarova@ispras.ru>
2023-09-07 15:41:31 +0000

    empty commit

--- src/pages/DictImport/index.js
+++ src/pages/DictImport/index.js
@@ -319,6 +319,7 @@ Info.propTypes = {
   setLicense: PropTypes.func.isRequired
 };
 
+
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
   graphql(fieldsQuery, { options: { fetchPolicy: "network-only" } }),
