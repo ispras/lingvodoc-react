0d5da857dced5f3d12333de887446488e5aad250
nataliauvarova <n.uvarova@ispras.ru>
2026-02-04 12:16:52 +0000

    grants

--- src/pages/Grants/index.js
+++ src/pages/Grants/index.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Button, Divider, Header, Message, Step } from "semantic-ui-react";
-import { graphql } from "@apollo/client/react/hoc";
-import Immutable, { fromJS } from "immutable";
-import PropTypes from "prop-types";
-import { branch, compose, pure, renderComponent, renderNothing, withProps } from "recompose";
 
 import GrantsList from "components/Grants/GrantsList";
 
-class Grants extends React.Component {
-  render() {
-    return (
-      <div className="background-content">
-        <GrantsList />
-      </div>
-    );
-  }
-}
+const Grants = () => {
+  return (
+    <div className="background-content">
+      <GrantsList />
+    </div>
+  );
+};
 
 Grants.propTypes = {};
 
