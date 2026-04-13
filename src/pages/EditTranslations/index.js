e948b11d66253e973a8987f4bce59caf4d7c06ef
nataliauvarova <n.uvarova@ispras.ru>
2026-04-13 15:09:33 +0000

    /edit_translations

--- src/pages/EditTranslations/index.js
+++ src/pages/EditTranslations/index.js
@@ -56,84 +56,79 @@ const Filter = ({ filterStr: initialFilterStr, caseSensitive, regularExpression,
   );
 };
 
-class EditTranslations extends React.Component {
-  constructor(props) {
-    super(props);
+const EditTranslations = ({ user }) => {
+  const getTranslation = useContext(TranslationContext);
 
-    this.state = {
-      selectedCategory: 0,
-      filterStr: "",
-      caseSensitive: false,
-      regularExpression: false
-    };
-  }
+  const [selectedCategory, setSelectedCategory] = useState(0);
+  const [filterStr, setFilterStr] = useState("");
+  const [caseSensitive, setCaseSensitive] = useState(false);
+  const [regularExpression, setRegularExpression] = useState(false);
 
-  handleCategoryClick = (e, { index }) => {
-    this.setState({ selectedCategory: index });
+  const handleCategoryClick = (e, { index }) => {
+    setSelectedCategory(index);
   };
 
-  render() {
-    const { user } = this.props;
-
-    if (user.id === undefined || user.id !== 1) {
-      return (
-        <div className="page-content">
-          <h4>{this.context("This page is available for administrator only")}</h4>
-        </div>
-      );
-    }
-
-    const { selectedCategory } = this.state;
-
+  if (user.id === undefined || user.id !== 1) {
     return (
-      <div className="lingvodoc-page">
-        <div className="lingvodoc-page__content">
-          <div className="background-header lingvo-translations-head">
-            <div className="lingvo-translations-menu">
-              <Menu secondary>
-                {categories.map((category, index) => (
-                  <Menu.Item
-                    key={index}
-                    name={this.context(category)}
-                    index={index}
-                    active={selectedCategory === index}
-                    onClick={this.handleCategoryClick}
-                  />
-                ))}
-              </Menu>
-            </div>
-          </div>
-
-          <Container>
-            <h1 className="lingvo-header-translations">
-              {this.context(categories[selectedCategory])}
-            </h1>
-
-            <Filter
-              filterStr={this.state.filterStr}
-              caseSensitive={this.state.caseSensitive}
-              regularExpression={this.state.regularExpression}
-              onChange={state => this.setState(state)}
-            />
-
-            {selectedCategory === -1 ? null : (
-              <TranslationsBlock
-                gists_type={selectedCategory === 7 ? "" : categories[selectedCategory]}
-                searchstring={this.state.filterStr}
-                search_case_insensitive={!this.state.caseSensitive}
-                search_regular_expression={this.state.regularExpression}
-              />
-            )}
-
-          </Container>
-
-        </div>
-        <Footer />
+      <div className="page-content">
+        <h4>{getTranslation("This page is available for administrator only")}</h4>
       </div>
     );
   }
-}
 
-EditTranslations.contextType = TranslationContext;
+  return (
+    <div className="lingvodoc-page">
+      <div className="lingvodoc-page__content">
+        <div className="background-header lingvo-translations-head">
+          <div className="lingvo-translations-menu">
+            <Menu secondary>
+              {categories.map((category, index) => (
+                <Menu.Item
+                  key={index}
+                  name={getTranslation(category)}
+                  index={index}
+                  active={selectedCategory === index}
+                  onClick={handleCategoryClick}
+                />
+              ))}
+            </Menu>
+          </div>
+        </div>
+
+        <Container>
+          <h1 className="lingvo-header-translations">{getTranslation(categories[selectedCategory])}</h1>
+          <Filter
+            filterStr={filterStr}
+            caseSensitive={caseSensitive}
+            regularExpression={regularExpression}
+            onChange={state => {
+              if (state.selectedCategory !== undefined) {
+                setSelectedCategory(state.selectedCategory);
+              }
+              if (state.filterStr !== undefined) {
+                setFilterStr(state.filterStr);
+              }
+              if (state.caseSensitive !== undefined) {
+                setCaseSensitive(state.caseSensitive);
+              }
+              if (state.regularExpression !== undefined) {
+                setRegularExpression(state.regularExpression);
+              }
+            }}
+          />
+          {selectedCategory === -1 ? null : (
+            <TranslationsBlock
+              gists_type={selectedCategory === 7 ? "" : categories[selectedCategory]}
+              searchstring={filterStr}
+              search_case_insensitive={!caseSensitive}
+              search_regular_expression={regularExpression}
+            />
+          )}
+        </Container>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default connect(state => state.user)(EditTranslations);
