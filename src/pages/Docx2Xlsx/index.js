f13c00ea731aaa5d0255595cabdbda1f3b956595
nataliauvarova <n.uvarova@ispras.ru>
2026-04-15 10:25:11 +0000

    /docx2eaf

--- src/pages/Docx2Xlsx/index.js
+++ src/pages/Docx2Xlsx/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { connect } from "react-redux";
 import { Button, Checkbox, Dimmer, Icon, Input, Label, List, Loader, Message, Segment } from "semantic-ui-react";
 import { gql } from "@apollo/client";
@@ -7,194 +7,230 @@ import { compose } from "recompose";
 
 import TranslationContext from "Layout/TranslationContext";
 
-const docx2xlsxMutation = gql`
-  mutation docx2xlsx(
+const docx2eafMutation = gql`
+  mutation docx2eaf(
     $docxFile: Upload
     $separateFlag: Boolean
+    $allTablesFlag: Boolean
+    $noHeaderFlag: Boolean
+    $noParsingFlag: Boolean
   ) {
-    docx2xlsx(
+    docx2eaf(
       docx_file: $docxFile
       separate_flag: $separateFlag
+      all_tables_flag: $allTablesFlag
+      no_header_flag: $noHeaderFlag
+      no_parsing_flag: $noParsingFlag
     ) {
       triumph
-      xlsx_url
+      eaf_url
+      alignment_url
+      check_txt_url
+      check_docx_url
       message
     }
   }
 `;
 
-class Docx2Xlsx extends React.Component {
-  constructor(props) {
-    super(props);
+const Docx2Eaf = ({ docx2eaf, loading, user }) => {
+  const getTranslation = useContext(TranslationContext);
 
-    this.state = {
-      separate_flag: false,
-      file: null,
-      converting_flag: false,
-      error_message: null,
-      result: null
-    };
+  const [separate_flag, setSeparate_flag] = useState(false);
+  const [all_tables_flag, setAll_tables_flag] = useState(false);
+  const [no_header_flag, setNo_header_flag] = useState(false);
+  const [no_parsing_flag, setNo_parsing_flag] = useState(false);
+  const [file, setFile] = useState(null);
+  const [converting_flag, setConverting_flag] = useState(false);
+  const [error_message, setError_message] = useState(null);
+  const [result, setResult] = useState(null);
 
-    this.onConvert = this.onConvert.bind(this);
-  }
+  const onConvert = () => {
+    docx2eaf({
+      variables: {
+        docxFile: file,
+        separateFlag: separate_flag,
+        allTablesFlag: all_tables_flag,
+        noHeaderFlag: no_header_flag,
+        noParsingFlag: no_parsing_flag
+      }
+    }).then(
+      ({ data: { docx2eaf } }) => {
+        setConverting_flag(false);
+        setResult(docx2eaf);
+      },
 
-  onConvert() {
-    this.props
-      .docx2xlsx({
-        variables: {
-          docxFile: this.state.file,
-          separateFlag: this.state.separate_flag
-        }
-      })
+      error_data => {
+        setConverting_flag(false);
+        setError_message(error_data.message);
+      }
+    );
 
-      .then(
-        ({ data: { docx2xlsx } }) => {
-          this.setState({
-            converting_flag: false,
-            result: docx2xlsx
-          });
-        },
+    setConverting_flag(true);
+    setError_message(null);
+    setResult(null);
+  };
 
-        error_data => {
-          this.setState({
-            converting_flag: false,
-            error_message: error_data.message
-          });
-        }
-      );
+  return (
+    <div className="background-content">
+      {user.id === undefined && !loading ? (
+        <Message>
+          <Message.Header>{getTranslation("Please sign in")}</Message.Header>
+          <p>{getTranslation("Only registered users can convert .docx to .eaf.")}</p>
+        </Message>
+      ) : loading ? (
+        <Segment>
+          <Loader active inline="centered" indeterminate>
+            {getTranslation("Loading")}...
+          </Loader>
+        </Segment>
+      ) : (
+        <Segment>
+          <List>
+            <List.Item>
+              <Checkbox
+                label={getTranslation("All tables")}
+                checked={all_tables_flag}
+                onChange={(e, { checked }) => {
+                  setAll_tables_flag(checked);
+                  setError_message(null);
+                  setResult(null);
+                }}
+              />
+            </List.Item>
 
-    this.setState({
-      converting_flag: true,
-      error_message: null,
-      result: null
-    });
-  }
+            <List.Item>
+              <Checkbox
+                label={getTranslation("Separate by paragraphs")}
+                checked={separate_flag}
+                onChange={(e, { checked }) => {
+                  setSeparate_flag(checked);
+                  setError_message(null);
+                  setResult(null);
+                }}
+              />
+            </List.Item>
 
-  render() {
-    const result = this.state.result;
+            <List.Item>
+              <Checkbox
+                label={getTranslation("No header")}
+                checked={no_header_flag}
+                onChange={(e, { checked }) => {
+                  setNo_header_flag(checked);
+                  setError_message(null);
+                  setResult(null);
+                }}
+              />
+            </List.Item>
 
-    return (
-      <div className="background-content">
-        {this.props.user.id === undefined && !this.props.loading ? (
-          <Message>
-            <Message.Header>{this.context("Please sign in")}</Message.Header>
-            <p>{this.context("Only registered users can convert .docx to .xlsx.")}</p>
-          </Message>
-        ) : this.props.loading ? (
-          <Segment>
-            <Loader active inline="centered" indeterminate>
-              {this.context("Loading")}...
-            </Loader>
-          </Segment>
-        ) : (
-          <Segment>
-            <List>
-              <List.Item>
-                <Checkbox
-                  label={this.context("Separate by alphabet")}
-                  checked={this.state.separate_flag}
-                  onChange={(e, { checked }) =>
-                    this.setState({
-                      separate_flag: checked,
-                      error_message: null,
-                      result: null
-                    })
-                  }
-                />
-              </List.Item>
-              <List.Item>
-                <span>
-                  {this.context(
-                    this.state.file ? ".docx file for convertion:" : "Please select .docx file for convertion."
-                  )}
-                </span>
+            <List.Item>
+              <Checkbox
+                label={getTranslation("No parsing")}
+                checked={no_parsing_flag}
+                onChange={(e, { checked }) => {
+                  setNo_parsing_flag(checked);
+                  setError_message(null);
+                  setResult(null);
+                }}
+              />
+            </List.Item>
 
-                {this.state.file && (
-                  <Label style={{ marginLeft: "0.5em" }}>
-                    <Icon name="file outline" />
-                    {this.state.file.name}
-                  </Label>
+            <List.Item>
+              <span>
+                {getTranslation(file ? ".docx file for convertion:" : "Please select .docx file for convertion.")}
+              </span>
+
+              {file && (
+                <Label style={{ marginLeft: "0.5em" }}>
+                  <Icon name="file outline" />
+                  {file.name}
+                </Label>
+              )}
+
+              <Button style={{ marginLeft: "1em" }} onClick={() => document.getElementById("file-select").click()}>
+                {`${getTranslation("Browse")}...`}
+              </Button>
+
+              <Input
+                id="file-select"
+                type="file"
+                style={{ display: "none" }}
+                onChange={e => {
+                  setFile(e.target.files[0]);
+                  setError_message(null);
+                  setResult(null);
+                }}
+              />
+            </List.Item>
+
+            <List.Item>
+              <Button color="green" content={getTranslation("Convert")} disabled={!file} onClick={onConvert} />
+            </List.Item>
+          </List>
+
+          {error_message && (
+            <Message negative>
+              <Message.Header>{getTranslation("Convertion error")}</Message.Header>
+              <p>
+                <span>{getTranslation("Please contact developers at")} </span>
+                <a href="https://t.me/lingvodoc_support" target="_blank" rel="noreferrer">
+                  {getTranslation("Support@Telegram")}
+                </a>
+                <span> {getTranslation("or at")} </span>
+                <a href="https://github.com/ispras/lingvodoc-react/issues">{getTranslation("Lingvodoc Github")}</a>
+                <span>.</span>
+              </p>
+              <p>{error_message}</p>
+            </Message>
+          )}
+
+          {converting_flag && (
+            <Dimmer active inverted>
+              <Loader inverted indeterminate>
+                {getTranslation("Converting")}...
+              </Loader>
+            </Dimmer>
+          )}
+
+          {result && !result.triumph && (
+            <Message>
+              <Message.Header>{getTranslation("Convertion failed")}</Message.Header>
+              <p>{getTranslation(result.message)}</p>
+            </Message>
+          )}
+
+          {result && result.triumph && (
+            <Message positive>
+              <Message.Header>{getTranslation("Converted successfully")}</Message.Header>
+              <List>
+                <List.Item>
+                  <a href={result.eaf_url}>{getTranslation(".eaf file")}</a>
+                </List.Item>
+
+                <List.Item>
+                  <a href={result.check_txt_url}>{getTranslation("check .txt")}</a>
+                </List.Item>
+
+                {result.check_docx_url && (
+                  <List.Item>
+                    <a href={result.check_docx_url}>{getTranslation("check .docx")}</a>
+                  </List.Item>
                 )}
 
-                <Button style={{ marginLeft: "1em" }} onClick={() => document.getElementById("file-select").click()}>
-                  {`${this.context("Browse")}...`}
-                </Button>
-
-                <Input
-                  id="file-select"
-                  type="file"
-                  style={{ display: "none" }}
-                  onChange={e =>
-                    this.setState({
-                      file: e.target.files[0],
-                      error_message: null,
-                      result: null
-                    })
-                  }
-                />
-              </List.Item>
-
-              <List.Item>
-                <Button
-                  color="green"
-                  content={this.context("Convert")}
-                  disabled={!this.state.file}
-                  onClick={this.onConvert}
-                />
-              </List.Item>
-            </List>
-
-            {this.state.error_message && (
-              <Message negative>
-                <Message.Header>{this.context("Convertion error")}</Message.Header>
-                <p>
-                  <span>{this.context("Please contact developers at")} </span>
-                  <a href="https://t.me/lingvodoc_support" target="_blank" rel="noreferrer">
-                    {this.context("Support@Telegram")}
-                  </a>
-                  <span> {this.context("or at")} </span>
-                  <a href="https://github.com/ispras/lingvodoc-react/issues">{this.context("Lingvodoc Github")}</a>
-                  <span>.</span>
-                </p>
-                <p>{this.context(this.state.error_message)}</p>
-              </Message>
-            )}
-
-            {this.state.converting_flag && (
-              <Dimmer active inverted>
-                <Loader inverted indeterminate>
-                  {this.context("Converting")}...
-                </Loader>
-              </Dimmer>
-            )}
-
-            {result && !result.triumph && (
-              <Message>
-                <Message.Header>{this.context("Convertion failed")}</Message.Header>
-                <p>{this.context(result.message)}</p>
-              </Message>
-            )}
-
-            {result && result.triumph && (
-              <Message positive>
-                <Message.Header>{this.context("Converted successfully")}</Message.Header>
-                <List>
+                {result.alignment_url && (
                   <List.Item>
-                    <a href={result.xlsx_url}>{this.context(".xlsx file")}</a>
+                    <a href={result.alignment_url}>{getTranslation("alignment .docx")}</a>
                   </List.Item>
-                </List>
-              </Message>
-            )}
-          </Segment>
-        )}
-      </div>
-    );
-  }
-}
-
-Docx2Xlsx.contextType = TranslationContext;
+                )}
+              </List>
+            </Message>
+          )}
+        </Segment>
+      )}
+    </div>
+  );
+};
 
 export default compose(
   connect(state => state.user),
-  graphql(docx2xlsxMutation, { name: "docx2xlsx" })
-)(Docx2Xlsx);
+  graphql(docx2eafMutation, { name: "docx2eaf" })
+)(Docx2Eaf);
